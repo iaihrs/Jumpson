c38eb1ae84b103275d0848856608e860dbf5cb03
iaihrs <146690632+iaihrs@users.noreply.github.com>
2023-10-12 20:34:00 +0100

    Add project files.

---
+++ Jumpson/JumpsonClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MelonLoader;
+using HarmonyLib;
+using System.Reflection;
+
+namespace Jumpson
+{
+    public class JumpsonClass : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                Type jump = typeof(PlayerMovement);
+                FieldInfo jumpForce = jump.GetField("jumpForce", BindingFlags.NonPublic | BindingFlags.Instance);
+                jumpForce.SetValue(7152, 1100);
+            }
+        }
+    }
+}
